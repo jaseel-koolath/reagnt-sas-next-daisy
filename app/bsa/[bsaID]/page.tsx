393966590e15a05606aa0f789cc1f7717a6f285c
jaseel-koolath <jaseel.koolath@gmai.com>
2023-08-16 19:04:58 +0530

    update home page to select users

--- app/bsa/[bsaID]/page.tsx
+++ app/bsa/[bsaID]/page.tsx
@@ -1,15 +1,59 @@
 import Image from "next/image";
+import Link from "next/link";
+
+interface GeoLocation {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: GeoLocation;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+const getUser = async (id: String): Promise<User> => {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await data.json();
+  return user;
+};
 
 export default async function Page({ params }: { params: { bsaID: string } }) {
+  const user = await getUser(params.bsaID);
   return (
     <div className="h-[100vh]">
+      <Link href="/">
+        <button className="btn btn-primary">Go back Home</button>
+      </Link>
       <button className="btn">Agent Id: {params.bsaID}</button>
-      <div className="badge">default</div>
-      <div className="badge badge-neutral">neutral</div>
-      <div className="badge badge-primary">primary</div>
-      <div className="badge badge-secondary">secondary</div>
-      <div className="badge badge-accent">accent</div>
-      <div className="badge badge-ghost">ghost</div>
+      <button className="btn">Agent Name: {user.name}</button>
+      <div>
+        <div className="badge">default</div>
+        <div className="badge badge-neutral">neutral</div>
+        <div className="badge badge-primary">primary</div>
+        <div className="badge badge-secondary">secondary</div>
+        <div className="badge badge-accent">accent</div>
+        <div className="badge badge-ghost">ghost</div>
+      </div>
       <div className="flex gap-3">
         {[...Array(5)].map((_, i) => (
           <div key={i} className="card w-96 bg-base-100 shadow-xl">
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <button className="btn">Jaseel</button>;
+  return (
+    <div className="flex flex-col">
+      <Link href="/bsa/1">
+        <button className="btn">Agent 1</button>
+      </Link>
+      <Link href="/bsa/2">
+        <button className="btn">Agent 2</button>
+      </Link>
+      <Link href="/bsa/3">
+        <button className="btn">Agent 3</button>
+      </Link>
+      <Link href="/bsa/4">
+        <button className="btn">Agent 4</button>
+      </Link>
+      <Link href="/bsa/5">
+        <button className="btn">Agent 5</button>
+      </Link>
+      <Link href="/bsa/6">
+        <button className="btn">Agent 6</button>
+      </Link>
+      <Link href="/bsa/7">
+        <button className="btn">Agent 7</button>
+      </Link>
+      <Link href="/bsa/8">
+        <button className="btn">Agent 8</button>
+      </Link>
+      <Link href="/bsa/9">
+        <button className="btn">Agent 9</button>
+      </Link>
+      <Link href="/bsa/10">
+        <button className="btn">Agent 10</button>
+      </Link>
+    </div>
+  );
 }
